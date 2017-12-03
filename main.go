88547572e87dfcdb6e9878881106b8b652c3c167
Gabriel Branchaud Rubenovitch <gabriel.branchaud@gmail.com>
2017-12-03 16:13:58 -0500

    inputs are in their own package.

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
   "fmt"
+  "github.com/gbranchaudrubenovitch/advent-of-code-2017/inputs"
 )
 
 func main() {
 	fmt.Println("--- Day 1: Inverse Captcha ---")
-  fmt.Println(fmt.Sprintf("  1) solution to captcha is %d", day1Part1(d01p1Input)))
+  fmt.Println(fmt.Sprintf("  1) solution to captcha is %d", day1Part1(inputs.Day01)))
 }
