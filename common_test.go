622cebb7b26825e2cb2f8a90fd1a21d2b8a0084a
Gabriel Branchaud Rubenovitch <gabriel.branchaud@gmail.com>
2017-12-07 21:49:17 -0500

    Day 6, part 1, tests pass, but not answer...

--- common_test.go
+++ common_test.go
@@ -9,3 +9,16 @@ func assertIntEquals(actual int, expected int, t *testing.T) {
 		t.Error("Expected", expected, "but got", actual)
 	}
 }
+
+func assertIntSliceEquals(actual []int, expected []int, t *testing.T) {
+	if len(actual) != len(expected) {
+		t.Error("Expected 2 arrays of the same length, but got", expected, "and", actual)
+		return
+	}
+	for i, v := range actual {
+		if v != expected[i] {
+			t.Error("Expected", expected, "but got", actual, "Slices differ at index", i)
+			return
+		}
+	}
+}
--- day06.go
+++ day06.go
@@ -1,5 +1,48 @@
 package main
 
+import (
+	"strings"
+)
+
 func day6Part1(input string) int {
-	return 0
+	banks := stringsContainingNumberToInts(strings.Fields(input))
+
+	previouslySeenBanks := make(map[string]bool)
+	previouslySeenBanks[intSliceToString(banks)] = true
+
+	cycleCount := 0
+	for true {
+		banks = day6Redistribute(banks)
+		cycleCount++
+
+		banksID := intSliceToString(banks)
+		if previouslySeenBanks[banksID] {
+			return cycleCount
+		}
+		previouslySeenBanks[banksID] = true
+	}
+
+	return -1
+}
+
+func day6Redistribute(banks []int) []int {
+	biggestBank := 0
+	for i, blocks := range banks {
+		if blocks > banks[0] {
+			biggestBank = i
+		}
+	}
+
+	blocksToRedistribute := banks[biggestBank]
+	banks[biggestBank] = 0
+
+	theOneReceivingTheNextBlock := biggestBank + 1
+	for blocksToRedistribute > 0 {
+		theOneReceivingTheNextBlock = theOneReceivingTheNextBlock % len(banks)
+		banks[theOneReceivingTheNextBlock]++
+		blocksToRedistribute--
+		theOneReceivingTheNextBlock++
+	}
+
+	return banks
 }
